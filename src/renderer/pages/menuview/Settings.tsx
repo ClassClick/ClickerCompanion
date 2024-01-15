9fd06776f78a4360adae3b290b5f6512608cd99a
Thijs Kamphuis <thijskamphuis@icloud.com>
2024-01-15 13:15:10 +0100

    Move to branch

--- src/renderer/pages/menuview/Settings.tsx
+++ src/renderer/pages/menuview/Settings.tsx
@@ -9,7 +9,7 @@ import MenuWrapper from '../../components/menuview/MenuWrapper';
 export default function Settings({ setCurrentPage }: Props) {
   return (
     <MenuWrapper setCurrentPage={setCurrentPage}>
-      <p>settings settings</p>
+      <p>settings</p>
     </MenuWrapper>
   );
 }
